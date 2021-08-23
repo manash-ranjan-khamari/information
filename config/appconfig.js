fb35cefcf9ace34d1786c53852f7b3ed0ecdc7be
Manash Khamari <manash.khamari@outlook.com>
2021-08-23 15:00:04 +0530

    ENV variabled added for PORT

--- config/appconfig.js
+++ config/appconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: 3001,
+    port: process.env.PORT || 3001,
     appUrl: 'http://localhost:3001',
     mongo: {
         url: 'mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true',
